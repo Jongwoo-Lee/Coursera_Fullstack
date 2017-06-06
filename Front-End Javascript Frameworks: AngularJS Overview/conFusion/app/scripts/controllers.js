2726882ebb5b081f404ab0168f27f7b3637e4b1f
jongwooME <jongwoolee2014@u.northwestern.edu>
2017-06-06 20:27:25 +0900

    REST API

--- Front-End Javascript Frameworks: AngularJS Overview/conFusion/app/scripts/controllers.js
+++ Front-End Javascript Frameworks: AngularJS Overview/conFusion/app/scripts/controllers.js
@@ -7,21 +7,10 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = false;
+            $scope.showMenu = true;
             $scope.message = "Loading ...";
-            $scope.dishes= {};
 
-            menuFactory.getDishes()
-            .then(
-                function(response){
-                  $scope.dishes = response.data;
-                  $scope.showMenu = true;
-                },
-                function(response){
-                  $scope.message = "Error: " +response.status + " " + response.statusText;
-                }
-            );
-
+            $scope.dishes= menuFactory.getDishes().query();
 
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -83,20 +72,10 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             $scope.dish = {};
-            $scope.showDish = false;
+            $scope.showDish = true;
             $scope.message = "Loading ...";
 
-            menuFactory.getDish(parseInt($stateParams.id,10))
-            .then(
-                function(response){
-                  $scope.dish = response.data;
-                  $scope.showDish = true;
-                },
-                function(response){
-                  $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id, 10)});
 
         }])
 
@@ -121,23 +100,11 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory',
         function($scope, menuFactory, corporateFactory) {
 
-            $scope.fdish = {};
-            $scope.showDish = false;
+            $scope.showDish = true;
             $scope.message = "Loading ...";
 
-            menuFactory.getDish(0)
-            .then(
-                function(response){
-                  $scope.fdish = response.data;
-                  $scope.showDish = true;
-                },
-                function(response){
-                  $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
-            var promo = menuFactory.getPromotion(0);
-            $scope.promo = promo;
+            $scope.fdish = menuFactory.getDishes().get({id:0});
+            $scope.promo = menuFactory.getPromotion(0);
 
             var chief = corporateFactory.getLeader(3);
             $scope.chief = chief;
